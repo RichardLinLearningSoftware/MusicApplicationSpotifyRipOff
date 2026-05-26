7e4b12057eaba853f9b6e5b7c0dea240a0a10d65
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-05-26 12:13:18 +0200

    Small commit
    Starting to create a simple navigation system for the UI

--- MusicApplication/Program.cs
+++ MusicApplication/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hi");
+﻿Console.WriteLine("Welcome to Spotify ripoff. Write 'help' for commands");
+
+while( true )
+{
+    string command = Console.ReadLine().ToString();
+
+    if(command == "help")
+    {
+        Console.WriteLine("yeah");
+    }
+    else
+    {
+        Console.WriteLine("Command '" + command+"' is not a command or misspelled");
+    }
+}
--- MusicApplication/obj/Debug/net10.0/MusicApplication.AssemblyInfo.cs
+++ MusicApplication/obj/Debug/net10.0/MusicApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaccb2f604429000807d0375df9d9046e643986")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
