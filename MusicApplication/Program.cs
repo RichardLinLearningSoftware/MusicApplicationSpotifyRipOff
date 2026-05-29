417dc5b8bc4f4b13654c70bf56ac06c6a0b5c7da
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-05-29 11:08:10 +0200

    Small commit
    -When using the command help you will get a list of commands. Its still unfinished. Not every commands exist and the commands listed do not work.

--- MusicApplication/Program.cs
+++ MusicApplication/Program.cs
@@ -6,7 +6,12 @@ while( true )
 
     if(command == "help")
     {
-        Console.WriteLine("yeah");
+        Console.WriteLine("-search :You can use this command to search for users, music, albums and muscisians");
+        Console.WriteLine("-login :You can use this command to login");
+        Console.WriteLine("-logout :While You are logged in you can use this command to logout");
+        Console.WriteLine("-login :You can use this command to login");
+        Console.WriteLine("-play :When you use this command youll need to give a song name or album name to play the song");
+        Console.WriteLine("-view :you use this command youll need to give the name of the album, playlist, song, user or musician to see their info");
     }
     else
     {
--- MusicApplication/obj/Debug/net10.0/MusicApplication.AssemblyInfo.cs
+++ MusicApplication/obj/Debug/net10.0/MusicApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaccb2f604429000807d0375df9d9046e643986")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4b12057eaba853f9b6e5b7c0dea240a0a10d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
